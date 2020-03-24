c8679dabdc0020099601f73818a1f85750d4aa38
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-24 19:22:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -88,7 +88,7 @@ def checkout(cart, coupons)
   
   total = 0
   counter = 0
-  while counter < cart.length do
+  while counter < final_cart.length do
     total += final_cart[counter][:price] * final_cart[counter][:count]
     counter += 1
   end
