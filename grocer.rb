330649f618cce78bc0bfabaaa84d84ecd7184601
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-24 19:20:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -83,7 +83,7 @@ end
 
 def checkout(cart, coupons)
   consolidate_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidate_cart)
+  couponed_cart = apply_coupons(consolidate_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
   total = 0
